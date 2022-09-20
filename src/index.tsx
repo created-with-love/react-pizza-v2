97a3430a75c53c88fedc178a01c4a34b59b8c0c6
Vladymyr Ivanchenko <vladymyr.ivanchenko@oracle.com>
2022-09-21 00:08:23 +0300

    feat: add basic styles, html and project settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+const rootElem = document.getElementById('root');
+
+if (rootElem) {
+  const root = ReactDOM.createRoot(rootElem);
+
+  root.render(
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+  );
+}
