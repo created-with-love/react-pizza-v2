6d8e73aee7f98b84b9755abdeb1ca708e8aafa1d
Vladymyr Ivanchenko <vladymyr.ivanchenko@oracle.com>
2022-09-22 11:45:46 +0300

    feat: add fetch data from mockApi server

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import PizzaBlock from './PizzaBlock';
-import pizzas from '../assets/pizzas.json';
+
+const instance = axios.create({
+  baseURL: 'https://632c18141aabd8373992d871.mockapi.io/',
+  timeout: 0,
+});
 
 export default function ProductListing() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    if (!pizzas.length) {
+      instance
+        .get('items')
+        .then(res => setPizzas(res.data))
+        .catch(console.log);
+    }
+  }, [pizzas]);
+
   return (
     <div className="content__items">
       {pizzas.map(pizza => (
