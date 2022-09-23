69c96dbbd4d9cc5ac6aaa99805320eaf583741fb
Vladymyr Ivanchenko <vladymyr.ivanchenko@oracle.com>
2022-09-24 00:32:37 +0300

    feat: add small fixes for cart links and styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,23 @@
+import { useEffect } from 'react';
 import { Categories, ProductListing, Sort } from 'components';
 
 export default function Home() {
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }, []);
+
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
       </div>
       <h2 className="content__title">Вся піца</h2>
       <ProductListing />
-    </>
+    </div>
   );
 }
